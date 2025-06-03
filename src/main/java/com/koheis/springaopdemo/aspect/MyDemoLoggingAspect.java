895a72c8dbe0faa427ea3728c52223052f9cd51b
kohei-s <koheisuzuki@me.com>
2025-06-03 15:54:11 +0200

    Modify pointcut expression

--- src/main/java/com/koheis/springaopdemo/aspect/MyDemoLoggingAspect.java
+++ src/main/java/com/koheis/springaopdemo/aspect/MyDemoLoggingAspect.java
@@ -11,7 +11,7 @@ public class MyDemoLoggingAspect {
     // this is where we add all of our related advices for logging
 
     // an @Before advice
-    @Before("execution(public void addAccount())")
+    @Before("execution(public void com.koheis.springaopdemo.dao.AccountDAO.addAccount())")
     public void beforeAddAccountAdvice() {
         System.out.println("\n=========>>> Executing @Before advice on method");
     }
