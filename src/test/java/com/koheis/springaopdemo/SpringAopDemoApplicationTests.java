a9ae7a284403a00bd130af235f4b0bbffb8e4194
kohei-s <koheisuzuki@me.com>
2025-06-03 14:47:06 +0200

    Initial commit

---
+++ src/test/java/com/koheis/springaopdemo/SpringAopDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koheis.springaopdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
