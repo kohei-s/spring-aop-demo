7c58c67b491dd1b2943e55a03457b7d31649c04e
kohei-s <koheisuzuki@me.com>
2025-06-23 12:20:53 +0200

    Place advices in separate aspects with order

--- src/main/java/com/koheis/springaopdemo/aspect/MyApiAnalyticsAspect.java
+++ src/main/java/com/koheis/springaopdemo/aspect/MyApiAnalyticsAspect.java
@@ -7,12 +7,12 @@ import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
-@Order(2)
-public class MyDemoLoggingAspect {
+@Order(3)
+public class MyApiAnalyticsAspect {
 
     @Before("com.koheis.springaopdemo.aspect.AopExpressions.forDaoPackageNoGetterSetter()")
-    public void beforeAddAccountAdvice() {
-        System.out.println("\n=========>>> Executing @Before advice on method");
+    public void performApiAnalytics(){
+        System.out.println("\n=========>>> Performing API analytics");
     }
 
 }
