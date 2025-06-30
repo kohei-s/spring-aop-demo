8b48c6cb67ef3e00fcd299be3d71b75c577a7f9f
kohei-s <koheisuzuki@me.com>
2025-06-30 14:58:57 +0200

    Add a method findAccounts() to AccountDAO

--- src/main/java/com/koheis/springaopdemo/dao/AccountDAOImpl.java
+++ src/main/java/com/koheis/springaopdemo/dao/AccountDAOImpl.java
@@ -3,6 +3,9 @@ package com.koheis.springaopdemo.dao;
 import com.koheis.springaopdemo.Account;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Repository
 public class AccountDAOImpl implements AccountDAO {
 
@@ -44,6 +47,23 @@ public class AccountDAOImpl implements AccountDAO {
         System.out.println(getClass() + ": setServiceCode()");
 
         this.serviceCode = serviceCode;
+    }
+
+    @Override
+    public List<Account> findAccounts() {
+        List<Account> myAccounts = new ArrayList<>();
+
+        // create sample accounts
+        Account temp1 = new Account("John", "Silver");
+        Account temp2 = new Account("Madhu", "Platinum");
+        Account temp3 = new Account("Luca", "Gold");
+
+        // add tehm to our account list
+        myAccounts.add(temp1);
+        myAccounts.add(temp2);
+        myAccounts.add(temp3);
+
+        return myAccounts;
     }
 
 }
