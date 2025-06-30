afb23d9a0f39d8a032903eae0d1f4a518edad65c
kohei-s <koheisuzuki@me.com>
2025-06-30 16:00:09 +0200

    Add @AfterReturning advice

--- src/main/java/com/koheis/springaopdemo/SpringAopDemoApplication.java
+++ src/main/java/com/koheis/springaopdemo/SpringAopDemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class SpringAopDemoApplication {
 
@@ -18,8 +20,21 @@ public class SpringAopDemoApplication {
 	public CommandLineRunner commandLineRunner(AccountDAO theAccountDAO, MembershipDAO theMembershipDAO) {
 
 		return runner -> {
-			demoTheBeforeAdvice(theAccountDAO, theMembershipDAO);
+			// demoTheBeforeAdvice(theAccountDAO, theMembershipDAO);
+			demoTheAfterReturningAdvice(theAccountDAO);
 		};
+	}
+
+	private void demoTheAfterReturningAdvice(AccountDAO theAccountDAO) {
+
+		// call the method to find the accounts
+		List<Account> theAccounts = theAccountDAO.findAccounts();
+
+		// display the accounts
+		System.out.println("\n\nMain Program: demoTheAfterReturningAdvice");
+		System.out.println("----");
+		System.out.println(theAccounts);
+		System.out.println("\n");
 	}
 
 	private void demoTheBeforeAdvice(AccountDAO theAccountDAO, MembershipDAO theMembershipDAO) {
@@ -32,7 +47,7 @@ public class SpringAopDemoApplication {
 		theAccountDAO.addAccount(myAccount, true);
 		theAccountDAO.doWork();
 
-		// call the accountdao getter/setter methods
+		// call the accountDao getter/setter methods
 		theAccountDAO.setName("foober");
 		theAccountDAO.setServiceCode("silver");
 
--- src/main/java/com/koheis/springaopdemo/aspect/MyDemoLoggingAspect.java
+++ src/main/java/com/koheis/springaopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,16 +2,59 @@ package com.koheis.springaopdemo.aspect;
 
 import com.koheis.springaopdemo.Account;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Aspect
 @Component
 @Order(2)
 public class MyDemoLoggingAspect {
+
+    // add anew advice for @AfterReturning on the findAccounts method
+    @AfterReturning(
+            pointcut = "execution(* com.koheis.springaopdemo.dao.AccountDAO.findAccounts(..))",
+            returning = "result"
+    )
+    public void afterReturningFindAccountsAdvice(JoinPoint theJoinPoint, List<Account> result) {
+
+        // print out which method we are advising on
+        String method = theJoinPoint.getSignature().toShortString();
+        System.out.println("\n======>>> Executing @AfterReturning on method: " + method);
+
+        // print out the results of the method call
+        System.out.println("\n======>>> result is:" + result);
+
+        // let's post-process the data ... let's modify it :-)
+
+        // convert the account names to uppercase
+        convertAccountNamesToUpperCase(result);
+
+        System.out.println("\n======>>> result is:" + result);
+    }
+
+    private void convertAccountNamesToUpperCase(List<Account> result) {
+
+        // loop through accounts
+        for (Account tempAccount : result) {
+
+            // get uppercase version of name
+            String theUpperName = tempAccount.getName().toUpperCase();
+
+            // update the name on the account
+            tempAccount.setName(theUpperName);
+
+        }
+
+
+
+    }
+
 
     @Before("com.koheis.springaopdemo.aspect.AopExpressions.forDaoPackageNoGetterSetter()")
     public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
