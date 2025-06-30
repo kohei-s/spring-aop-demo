746df88efe8e2921390f40e4aa75fd039e7a3b8c
kohei-s <koheisuzuki@me.com>
2025-06-30 14:52:26 +0200

    Add constructors and toString to Account.java

--- src/main/java/com/koheis/springaopdemo/Account.java
+++ src/main/java/com/koheis/springaopdemo/Account.java
@@ -7,6 +7,14 @@ public class Account {
     private String level;
 
 
+    public Account() {
+    }
+
+    public Account(String name, String level) {
+        this.name = name;
+        this.level = level;
+    }
+
     public String getName() {
         return name;
     }
@@ -22,4 +30,13 @@ public class Account {
     public void setLevel(String level) {
         this.level = level;
     }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "name='" + name + '\'' +
+                ", level='" + level + '\'' +
+                '}';
+    }
+
 }
